1f54b1393200441c1f5ec0fce40a63bc45568d80
Sela Mador-Haim <selama@gmail.com>
2022-10-03 01:02:41 -0400

    Index function returns an integer

--- unQLite/include/TemplateQuery.h
+++ unQLite/include/TemplateQuery.h
@@ -1024,7 +1024,7 @@ public:
     virtual string getString(TQContext& ctx);
 };
 
-class TExprIndex: public TExprString
+class TExprIndex: public TExpression
 {
 public:
     virtual bool isInt(TQContext* ctx) {return true;}
