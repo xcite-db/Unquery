824b3719a17172594e9afb252ddb391b5b8fb303
Sela Mador-Haim <selama@gmail.com>
2022-10-20 11:18:07 -0400

    Few more tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,11 +3,22 @@ UNQ=../unq/bin/Release/unq
 JSONCOMPARE=../JSONCompare/bin/Release/JSONCompare
 EMPLOYEES=../tutorial-samples/employees
 
+function test_query() {
+    basefile=$2${1##*/}
+    $UNQ -f $1 ${@:3} >results/$basefile 2> results/${basefile}.errors
+    if [ -s expected/$basefile ]; then
+       $JSONCOMPARE expected/$basefile results/$basefile
+    fi
+    diff -Naur expected/$basefile.errors results/$basefile.errors || true
+}
+
+
 rm -rf results
 mkdir results
 for f in ${EMPLOYEES}/queries/*.unq; do
-    basefile=employee_${f##*/}
-    $UNQ -f $f ${EMPLOYEES}/employee*.json >results/$basefile 2> results/${basefile}.errors
-    $JSONCOMPARE expected/$basefile results/$basefile
-    diff -Naur expected/$basefile.errors results/$basefile.errors
+    test_query $f employee_ $EMPLOYEES/employee*.json
+done
+
+for f in parsing/*.unq; do
+    test_query $f parsing_ $EMPLOYEES/employee1.json
 done
